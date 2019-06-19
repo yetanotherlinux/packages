ee10dac5850ab6bab29f6119f33156c908a12904
Your Name <you@example.com>
2019-06-20 00:56:34 +0300

    .

---
+++ .sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+git checkout master
+git pull
+
+branches=( x86_64 aarch64 )
+for i in "${branches[@]}"
+do
+	git checkout $i
+	git pull
+	git rebase master
+	git push --force
+done
+
+git checkout master
+git push
